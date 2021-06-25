7c6b4371ae45d3adbee6ae253db992d6fdf79085
manish22127 <78017202+manish22127@users.noreply.github.com>
2021-06-25 14:31:48 +0530

    Update yd.py
    some new print statements changes

--- yd.py
+++ yd.py
@@ -1,7 +1,11 @@
 
+print("welcome to youtube downloader")
+
+
 from pytube import YouTube
-vedio = None
 link = input("enter the url")
 vedio = YouTube(link)
 stream = vedio.streams.get_highest_resolution()
+print("Download initialised")
 stream.download()
+print("Thankyou for downloading \nProgram wirtten by Manish");
